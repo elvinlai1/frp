5651b27726dcb4b00bf7b10aabbcf0bfeb59adbe
Boredest <hodge195@gmail.com>
2022-02-09 19:18:41 -0800

    Django base
    Django with templates.

---
+++ facialRecognition/facialRecognition/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage (request):
+   # return HttpResponse("main")
+   return render(request, 'homepage.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def timesheet(request):
+   return render(request, 'timesheets.html')
+
+def profile(request):
+  return render(request, 'profile.html')
+
+def form(request):
+    return render(request, 'form.html')
