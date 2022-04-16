cc625e252b83b8916bff19e8b37965a9aacf8221
Elvin Lai <61441620+elvinlai1@users.noreply.github.com>
2022-04-15 22:10:58 -0700

    changed TZ and small fixes

--- frpApp/test.py
+++ frpApp/test.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import time
 from Database import *
 
 db =  Database()
@@ -16,8 +18,15 @@ print('\n')
 
 db3 = Database()
 getLastts = db3.get_LastTimestamp(111)
-#print(getLastts)
+print(getLastts)
 
 employees =  db.get_Employee(111)[0]
 
 print(employees)
+
+
+dt = datetime.now()
+
+ts = datetime.timestamp(dt)
+local_time = dt.strftime("%H:%M:%S")
+print(local_time)
